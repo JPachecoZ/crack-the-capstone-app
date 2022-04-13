3b2092fc9e06d5a9d74bf13b74ffee0e1b5f20cc
Alonso <alonsozf.94@gmail.com>
2022-04-13 18:11:41 -0500

    Added style to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
-import { findByLabelText } from '@testing-library/react';
 import React, { useState } from 'react';
+import { Button, Typography } from '@mui/material'
+import UploadOutlinedIcon from '@mui/icons-material/UploadOutlined';
+import styled from "@emotion/styled"
+import '@fontsource/roboto/300.css';
 
+type ChangeEvent = React.ChangeEvent<HTMLInputElement>
+type ButtonElement = React.FormEvent<HTMLButtonElement>
+
+const Container = styled.div`
+  background: rgba(0, 0, 0, 0) linear-gradient(315deg, rgb(187, 255, 253) 0.01%, rgb(89, 111, 255) 100%) repeat scroll 0% 0%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  color: #fff;
+`
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  text-align: center;
+`
+
+const UploadForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+function UploadButton({onChange} :any) {
+  return (
+    <Button
+      variant="contained"
+      component="label"
+      style={{
+        background: 'rgba(0, 0, 0, 0.5)'
+      }}
+    >
+       <UploadOutlinedIcon />
+       <input onChange={onChange} type="file" hidden/>
+    </Button>
+  )
+}
+  
 function App(): JSX.Element {
 
   const [file, setFile] = useState<File>();
 
-  type ChangeEvent = React.ChangeEvent<HTMLInputElement>
-  type ButtonElement = React.FormEvent<HTMLButtonElement>
-
   function handleData(event: ChangeEvent){
     event.preventDefault();
     if (event.target.files === null) return
+    console.log("File acquired")
     setFile(event.target.files[0]);
   }
 
@@ -32,22 +75,27 @@ function App(): JSX.Element {
     )
   }
 
-  const ContainerStyle = {
-    backgroundColor: "#333333",
-    height: "100vh",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "white"
-  }
-
   return (
-    <div style={ContainerStyle}>
-      <form>
-        <input onChange={(event: ChangeEvent) => handleData(event)} type="file"/>
-        <button onClick={(event: ButtonElement) => handleSubmit(event)}>Send Files</button>
-      </form>
-    </div>
+    <Container>
+      <TitleContainer>
+        <Typography variant="h2" sx={{fontWeight: '600'}}>CRACK THE CODE</Typography>
+        <Typography variant="h5">Massive Upload Platform</Typography>
+      </TitleContainer>
+      <UploadForm>
+        <UploadButton onChange={(event: ChangeEvent) => handleData(event)} />
+        {/* <input onChange={(event: ChangeEvent) => handleData(event)} type="file"/> */}
+        <Button
+          onClick={(event: ButtonElement) => handleSubmit(event)}
+          sx={{
+            padding: '5px 15px',
+            backgroundColor: '#fff',
+            color: '#000',
+            fontFamily: 'Roboto',
+            fontSize: '1.1rem'
+          }}
+        >Next</Button>
+      </UploadForm>
+    </Container>
   );
 }
 
